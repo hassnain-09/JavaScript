ec8302b64292d8c2eee0aa112a6189c424bd6eda
alikhan_09 <reachhassnainali@gmail.com>
2025-08-02 16:07:27 +0500

    dataTypes notes

---
+++ basics_01/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// ===
+
+//Important Notes
+//anything compared with NaN will always return false
+// ANYTHING compared with undefined will always return false
+// null is equal to undefined only with == operator, but not with === operator and also not equal to any other value like 0, "",false,true etc
